9a60d059d80c5061bf0d4534361b10c4bcb8530a
anmehta26 <anmehta26@utexas.edu>
2024-05-07 21:10:11 -0500

    fixed issue #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-const Hello = (props) => {
+const Header = (props) => {
   return (
-    <div>
-      <p>Hello {props.name}!</p>
-      <p>You are {props.age} years old!</p>
-    </div>
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises}</p>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = '20'
-  const friends = [
-    {name: 'Maya', age: 20},
-    {name: 'Ben', age: 19}
-  ]
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <h1>Greetings!</h1>
-      <Hello name={name} age={age} />
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-    </>
+    <div>
+      <Header course={course}/>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
+    </div>
   )
 }
 
